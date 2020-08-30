66787ab280ea1224a029c8de04808ba80aca171a
Damian <damina.jorga@wp.pl>
2020-08-30 18:16:56 +0200

    navigation fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 // navigation hamburger
-const button = document.querySelector(".mobile-menu_button");
-button.addEventListener("click", mobileMenu);
+// const button = document.querySelector(".mobile-menu_button");
+// button.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-const mobileLinks = document.querySelector(".menu-mobile");
-mobileLinks.classList.toggle("opened");
-}
+// function mobileMenu() {
+// const mobileLinks = document.querySelector(".menu-mobile");
+// mobileLinks.classList.toggle("opened");
+// }
 
 // icons arrow
 $(function(){
@@ -22,3 +22,26 @@ $(function(){
 		$('html').animate({scrollTop: 0}, 400, 'linear');
 	});
 });
+
+const hamburger=document.querySelector(".hamburger"),nav=document.querySelector(".navigation"),
+navItems=document.querySelectorAll(".navigation__item"),
+links=document.querySelectorAll("a")
+var percentage=0;function getScrollPercent(){
+	const e=document.documentElement,t=document.body,a="scrollTop",c="scrollHeight";
+percentage=Math.round((e[a]||t[a])/((e[c]||t[c])-e.clientHeight)*100)}
+function hideLangSwitch(){
+	percentage>3&&window.screen.width<992?langSwitch.style.opacity=0:langSwitch.style.opacity=1}
+function handleClick(){hamburger.classList.toggle("hamburger--active"),
+hamburger.setAttribute("aria-expanded",
+hamburger.classList.contains("hamburger--active")),
+nav.classList.toggle("navigation--active"),
+socialItems.forEach(e=>{e.classList.toggle("social__item--dark-mode-mobile")})}
+function closeMenu(){hamburger.classList.toggle("hamburger--active"),
+hamburger.setAttribute("aria-expanded",
+hamburger.classList.contains("hamburger--active")),
+nav.classList.toggle("navigation--active"),
+window.screen.width<992&&socialItems.forEach(e=>{e.classList.toggle("social__item--dark-mode-mobile")})}
+	hamburger.addEventListener("click",handleClick),
+	links.forEach(e=>e.addEventListener("click",closeMenu)),
+	window.addEventListener("scroll",changeNavColor);
+
